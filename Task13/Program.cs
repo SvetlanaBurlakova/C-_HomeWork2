3f291a31a1ac8b70dc960353d9a36f2abb6c2d40
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-30 13:38:42 +0400

    Task13 modified

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,17 +2,7 @@
 Console.WriteLine("Введите число:");
 int number=Convert.ToInt32(Console.ReadLine());
 
-bool ThirdDigit(int number)
-{
-    if (number/100>0)
-        {
-            return true;  
-        }
-    else return false;
-}
-bool thirdDigit=ThirdDigit(number);
-
-if (thirdDigit==false) Console.WriteLine("Третьей цифры нет");
+if (number/100==0) Console.WriteLine("Третьей цифры нет");
 else {
     int numberForCycle=number;
     while (numberForCycle/100>10) numberForCycle=numberForCycle/10;
