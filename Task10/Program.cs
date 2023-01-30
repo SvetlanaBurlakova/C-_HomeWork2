240220205dd3f67ac2c61be2ecbbfc989c014499
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-30 12:51:09 +0400

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int number=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"вторая цифра от числа {number} равна {(number/10)%10}");
