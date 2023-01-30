7e42901e7e9b67efba351e58d3aa556d9aa7b1f7
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-30 13:32:29 +0400

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int number=Convert.ToInt32(Console.ReadLine());
+
+if (number==6 || number==7) Console.WriteLine("да");
+else Console.WriteLine("нет");
